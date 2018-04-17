f3f29918a555da7471578b0a87f37d1f170399f2
Andika Ahmad Ramadhan <aspendaka@gmail.com>
2018-04-18 05:12:50 +0700

    Add Categories Controller, Model, Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Elektronik")
+Category.create(name: "Handphone")
+Category.create(name: "Laptop")
+Category.create(name: "Radio")
